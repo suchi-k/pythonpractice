e5fd5d3e3e52883797048bf477d99d83f42e8ff1
K Sucharitha <ksucharitha131097@gmail.com>
2024-02-02 19:23:08 +0530

    added 23,24,25,26 files

---
+++ python_learnings_telusko/24_for_else.py
@@ -0,0 +1,14 @@
+comp = [10,15,23,54]
+for i in comp:
+    if i%5==0:
+        print(i)
+        break
+    else:
+        print("Not found")
+
+for i in comp:
+    if i%5==0:
+        print(i)
+        
+else:
+    print("Not found")          
---
+++ python_learnings_telusko/25_prime_numbers.py
@@ -0,0 +1,7 @@
+nums = 15
+for i in range(2,nums):
+    if nums % i == 0:
+        print('Not prime')
+
+else:
+    print('prime')        
