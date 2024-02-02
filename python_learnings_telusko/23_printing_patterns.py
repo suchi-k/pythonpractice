95cc1bd284b265f0cde620e1b95f70b0bc865659
K Sucharitha <ksucharitha131097@gmail.com>
2024-02-02 23:39:41 +0530

    modified code

--- python_learnings_telusko/23_printing_patterns.py
+++ python_learnings_telusko/23_printing_patterns.py
@@ -1,10 +1,15 @@
-# print('#',end ='')
-# print('#',end ='')
-# print('#',end ='')
-# print('#',end ='')
+"""
+Always print statements print the messages in new lines
+If we want to print any values in a single line using print()
+we need to use end argument
+"""
+print('#', end=',')
+print('#', end=',')
+print('#', end='')
+print('#', end='')
 
 # for i in range(4):
-#     print('#',end ='')
+#     print('#', end ='')
 
 # print()
 
@@ -23,8 +28,8 @@
 
 # print()
 
-for i in  range(4):
-    for j in range(4-i):
-        print("#" , end ="")
+# for i in  range(4):
+#     for j in range(4-i):
+#         print("#" , end ="")
 
-    print()
+#     print()
--- python_learnings_telusko/25_prime_numbers.py
+++ python_learnings_telusko/25_prime_numbers.py
@@ -1,7 +1,56 @@
-nums = 15
-for i in range(2,nums):
-    if nums % i == 0:
-        print('Not prime')
+"""
+Explanation: A prime number is a natural number greater than 1 that
+has no positive divisors other than 1 and itself.
+The first few prime numbers are {2, 3, 5, 7, 11, ….}. 
+"""
 
-else:
-    print('prime')        
+from math import sqrt
+
+nums = 15
+
+
+def is_prime(number):
+    """
+    1) The idea to solve this problem is to iterate through 
+    all the numbers starting from 2 to (N/2) using a for loop.
+    
+    2) For every number, check if it divides N.
+    
+    3) If we find any number that divides N, we return False.
+    
+    4) If we did not find any number between 2 and N/2 which divides N 
+    then it means that N is prime and we will return True.
+    """
+    if number <= 1:
+        print(f"{number} is not a Prime Number")
+        return False
+    
+    # logic to check prime or not
+    for i in range(2, int(number/2) + 1):
+        if number % i == 0:
+            print(f"{number} is not a Prime Number")
+            return False
+    
+    print(f"{number} is a Prime Number")
+    return True
+
+print(is_prime(nums))
+
+def is_prime(number):
+    """
+    Instead of checking till N, we can check till √N because 
+    a larger factor of n must be a multiple of a smaller factor 
+    that has been already checked. 
+    Now let’s see the code for the first optimization method ( i.e. checking till √n )
+    """
+    if number <= 1:
+        return f"{number} is not a Prime Number"
+    
+    # logic to check prime or not
+    for i in range(2, int(sqrt(number)) + 1):
+        if number % i == 0:
+            return f"{number} is not a Prime Number"
+    
+    return f"{number} is a Prime Number"
+
+print(is_prime(nums))
