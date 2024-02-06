4958f8bade31b5b1af3c67d2bb61750a510f6a0f
K Sucharitha <ksucharitha131097@gmail.com>
2024-02-06 10:28:29 +0530

    we added 29,30 files

---
+++ python_learnings_telusko/29_ways _of creating arrays_numpy.py
@@ -0,0 +1,12 @@
+'''
+In Numpy is you put the array have allows same elements doesnt allows different values
+but in numpy the array function we give the elements in the int type that numpy convert float type.
+linspace you create the parts and break down range number of the parts
+logspace between the gap two elements will be the same
+zeros function says size 
+ones function says 0,1,you put the (5,int)that case i used intgers that function execute.
+'''
+
+from Numpy import *
+# arr = array([1,2,3,4,5],float)for example
+arr = linspace()
