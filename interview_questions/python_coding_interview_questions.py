9d1a2927c7c207de5c60dfa6bb4ac35fe3b8a2bd
K Sucharitha <ksucharitha131097@gmail.com>
2024-04-13 14:35:09 +0530

    new files added

--- interview_questions/python_coding_interview_questions.py
+++ interview_questions/python_coding_interview_questions.py
@@ -105,4 +105,111 @@ print(list(filter(lambda x:x%2==0, range(n*2))))
 Question:4
 Format given list into dict using index as keys and elements as values
 """
-l = [01, 20, 03, 40, 05, 60]
+
+nums = [3,1,2,4]
+l1=[76, 23, 45, 12, 54, 9] 
+print("Original List:", l1)
+ 
+# sorting list using nested loops without used built in sort function
+for i in range(0, len(l1)):
+    for j in range(i+1, len(l1)):
+        if l1[i] >= l1[j]:
+            l1[i], l1[j] = l1[j],l1[i]
+print("Sorted List", l1)
+
+sa = [5,1,2,4,3]
+print("original list:",sa)
+for i in range(0,len(sa)):
+    for j in range(i+1,len(sa)):
+        if sa[i] >= sa[j]:
+            sa[i],sa[j] = sa[j],sa[i]
+print("sorted list",sa)
+sa.sort()
+print(sa)
+
+'''
+when we use @classmethod and @staticmethod 
+
+We generally use the class method to create factory methods. Factory methods 
+return class objects ( similar to a constructor ) for different use cases.
+We generally use static methods to create utility functions.
+'''
+
+class Person:
+    @ classmethod
+    def math(cls,marks):
+        print('hi')
+
+    @ staticmethod
+    def subject(science,hindi):
+        print(f"{science} is a 35/100 marks,{hindi} is a 45/100")
+
+Person.math(45)  
+Person.subject(35,45)        
+# obj = Person()
+# obj.math(45)
+Person.math(45) 
+# obj = subject()
+# obj.subject(35,45)
+
+class Method:
+    def __init__(self,name):
+        self.name = name
+        print("This is constructor method")
+
+obj = Method('dasd')  
+
+class A:
+    def __init__(self, name,age):
+        self.name = name
+        self.age = age
+
+    def m(self):
+        print("This is instance method")
+
+obj = A('harika',21)
+obj.m()
+
+# class methods
+
+class A:
+    # This is constructor method
+    def __init__(self, name):
+        self.name = name
+        print('Hi') 
+
+class B(A):
+    def m1(self):
+        print('This is method overridding')
+    
+    # if you have same function then python execute last one
+    def m1(self):
+        print('This is a cat') 
+
+obj = A('')  
+obj = B('')
+obj.m1()
+
+class M:
+    @ staticmethod
+    def test(self):
+        print("This is a static method")
+        
+    @ classmethod
+    def test1(cls,name):
+        print("This is a class method")
+
+M.test('hi') 
+obj = M()
+obj. test('hi')
+obj.test1('suchi')
+
+
+"""Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+Input: s = "Let's take LeetCode contest"
+Output: "s'teL ekat edoCteeL tsetnoc"
+"""
+
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return ' '.join([i[::-1] for i in s.split()])
--- python_learnings_telusko/53_Types_of_methods.py
+++ python_learnings_telusko/53_Types_of_methods.py
@@ -1,19 +1,25 @@
-class student:
-    school = 'suchi' #class method
+class Student:
+    # class variable
+    school = 'suchi' 
+
     def __init__(self,m1,m2,m3): 
         self.m1 = m1
         self.m2 = m2
         self.m3 = m3
+
     def avg(self):
         return (self.m1+self.m2+self.m3)/3
 
     def get_m1(self):
         return self.m1
+    
     def set_m1(self,value):
         self.m1 = value     
+
     @classmethod
     def getschool(cls):
         return cls.school
+    
     @staticmethod
     def info():
         print("This is student class..in abc molude")
@@ -22,11 +28,7 @@ class student:
 # instance methods work instance varaibles
 # work extra varaibles if you want class varaibles instance varaibles go to
 #the static methods.
-                            
 
-    
-           
-
-s1 = student(34,35,65)
-s2 = student(89,65,66)
-student.info()
+s1 = Student(34,35,65)
+s2 = Student(89,65,66)
+Student.info()
